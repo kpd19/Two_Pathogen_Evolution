9b49caffa6b54e4fb7a229cdd52eea4324d0b605
Katie Dixon <kpd19@case.edu>
2025-06-24 12:13:52 -0500

    Adding missing figures to script

--- MorphotypeFrequency/aggregating_morphotype_data.R
+++ MorphotypeFrequency/aggregating_morphotype_data.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(gridExtra)
 library(mgcv)
 
-site_inf <- read_csv("MorphotypeFrequency/data/site_infection_data_2019-2020.csv")
-ll_data <- read_csv("MorphotypeFrequency/data/literature_dist_data.csv")
-pd_data <- read_csv("data/data_for_ll.csv")
+site_inf <- read_csv("morphotype_dist/data/site_infection_data_2019-2020.csv")
+ll_data <- read_csv("morphotype_dist/data/literature_dist_data.csv")
+pd_data <- read_csv("morphotype_dist/data/forest_composition_wide.csv")
+tree_sp <- read_csv("morphotype_dist/tree_polygons/tree_sp_df.csv")
 
 # calculating the infection totals for all the natural sites that weren't included in biopesticide spray projects
 site_inf_totals <- site_inf %>% filter(sprayed == FALSE) %>% group_by(state,latitude,longitude,site,year) %>%
@@ -26,10 +27,10 @@ site_inf_totals %>% ungroup() %>% summarize(sum_coinf = sum(coinf),
                                             sum_MNPV = sum(MNPV),
                                             sum_SNPV = sum(SNPV))
 
-ll_small <- pd_data %>% select(Site,Douglas_fir)
+ll_small <- pd_data %>% rename(Douglas_fir = `Douglas-fir`, Site = site) %>% select(Site,Douglas_fir)
 # analysing coinfections
 
-site_inf_totals2 <- merge(site_inf_totals,pd_data,by.x = c('Site'), by.y = c('Site'))
+site_inf_totals2 <- merge(site_inf_totals,ll_small)
 
 crit <- 1.96
 
@@ -41,6 +42,15 @@ df1 <- data.frame(rat = x1, y = pred1$fit, se = pred1$se.fit)
 df1 <- df1 %>% mutate(upper = y + 1.96*se,
                      lower = y - 1.96*se)
 p1 <- summary(mod1)$coefficients[2,4]
+
+site_inf_totals2 %>% ungroup() %>% filter(sum_infected >0) %>% 
+  ggplot() +
+  geom_ribbon(data = df1, aes(x = rat, ymin = lower, ymax = upper),
+              fill = 'blue', alpha = 0.25, linetype = 'dashed', color = 'grey55')+
+  geom_point(aes(x = sum_total,y = p_coinf), size = 2) + theme_classic(base_size = 15) + 
+  xlab("Non-dominant:Dominant") + ylab("Proportion Coinfected") +
+  geom_line(data = df1, aes(x = rat, y = y), color = 'blue', size = 1.25)     + 
+  annotate(geom = 'text', x = -Inf, y = Inf, label =  paste0("P-val = ", round(p1,6)), hjust = -0.15, vjust = 1)
 
 plt1 <- site_inf_totals2 %>% ungroup() %>% filter(sum_infected >0) %>% 
   ggplot() +
@@ -83,7 +93,7 @@ plt3 <- site_inf_totals2 %>% ungroup() %>% filter(sum_infected >0) %>%
   geom_ribbon(data = df3, aes(x = pMNPV, ymin = lower, ymax = upper),
               fill = 'blue', alpha = 0.25, linetype = 'dashed', color = 'grey55')+
   geom_point(aes(x = pMNPV,y = p_coinf), size = 2) + theme_classic(base_size = 15) + 
-  xlab("Fraction Infected") + ylab("Proportion Coinfected") +
+  xlab("Fraction Infected with\n Multi-capsid morphotype") + ylab("Proportion Coinfected") +
   geom_line(data = df3, aes(x = pMNPV, y = y), color = 'blue', size = 1.25)   + 
   annotate(geom = 'text', x = -Inf, y = Inf, label =  paste0("P-val = ", round(p3,3)), hjust = -0.15, vjust = 1)
 
@@ -101,7 +111,7 @@ plt4 <- site_inf_totals2 %>% ungroup() %>% filter(sum_infected >0) %>%
   geom_ribbon(data = df4, aes(x = pSNPV, ymin = lower, ymax = upper),
               fill = 'blue', alpha = 0.25, linetype = 'dashed', color = 'grey55')+
   geom_point(aes(x = pSNPV,y = p_coinf), size = 2) + theme_classic(base_size = 15) + 
-  xlab("Fraction Infected") + ylab("Proportion Coinfected") +
+  xlab("Fraction Infected with\n Single-capsid morphotype") + ylab("Proportion Coinfected") +
   geom_line(data = df4, aes(x = pSNPV, y = y), color = 'blue', size = 1.25)    + 
   annotate(geom = 'text', x = -Inf, y = Inf, label =  paste0("P-val = ", round(p4,3)), hjust = -0.15, vjust = 1)
 
@@ -144,8 +154,8 @@ plt6 <- site_inf_totals2 %>% ungroup() %>% filter(sum_infected >0) %>%
   annotate(geom = 'text', x = -Inf, y = Inf, label =  paste0("P-val = ", round(p6,3)), hjust = -0.15, vjust = 1)
 
 
-pdf("MorphotypeFrequency/figures/prop_coinf_ratio.pdf",height = 8, width = 12)
-grid.arrange(plt2, plt1,plt5,plt6, plt3,plt4,nrow=2)
+pdf("morphotype_dist/figures/prop_coinf_ratio.pdf",height = 8, width = 12)
+grid.arrange(plt2, plt1,plt5, plt3,plt4, plt6,nrow=2)
 dev.off()
 
 site_summary <- site_inf_totals %>% filter(total_iso >0) %>% arrange(Site) %>%
@@ -155,4 +165,57 @@ site_summary$site_no <- 111:128
 
 ll_data <- rbind(ll_data,site_summary)
 
-write_csv(ll_data, "MorphotypeFrequency/data/morphotye_distribution_data.csv")
+write_csv(ll_data, "morphotype_dist/data/morphotye_distribution_data.csv")
+
+head(ll_data)
+head(pd_data)
+
+pd_data2 <- pd_data %>% rename(Douglas_fir = `Douglas-fir`) %>% select(site,Douglas_fir)
+
+ll_data2 <- merge(ll_data,pd_data2, all = TRUE)
+
+site1 <- ll_data2 %>% ggplot() + aes(x = MNPV/(SNPV + MNPV)) + geom_histogram(bins = 11) + theme_classic(base_size = 15) + 
+  xlab("% Multi-capsid morphotype") + ylab("# Sites")
+
+site2 <- ll_data2 %>% ggplot() + aes(x = SNPV + MNPV) + geom_histogram(bins = 10) + theme_classic(base_size = 15) + 
+  xlab("# Virus IDs") + ylab("# Sites") + 
+  scale_x_log10()
+
+site3 <- ll_data2 %>% ggplot() + aes(x = Douglas_fir*100) + geom_histogram(bins = 11) + theme_classic(base_size = 15) + 
+  xlab("% Douglas-fir") + ylab("# Sites")
+
+site4 <- ll_data2 %>% ggplot() + aes(x = state) + geom_bar() + theme_classic(base_size = 15) + 
+  xlab("State") + ylab("# Sites")
+
+pdf("morphotype_dist/figures/site_info.pdf", height = 5, width = 15)
+grid.arrange(site1,site2,site3,site4,nrow = 1)
+dev.off()
+
+pd_long <- pd_data %>% pivot_longer(colnames(pd_data)[11:38])
+
+pd_long <- merge(pd_long,tree_sp)
+
+pdf("morphotype_dist/figures/dist_tree_sp_by_sp.pdf",height = 10, width = 10)
+pd_long %>% ggplot() + aes(x = site_no, y = value, fill = genus) + geom_bar(stat = 'identity', position = 'stack') + 
+  theme_classic(base_size = 9) + 
+  facet_wrap(~name,nrow = 7) + 
+  theme(legend.position = 'top') + 
+  scale_fill_discrete("") + 
+  ylab("% of tree species") + xlab("Site no.")
+dev.off()
+
+prop_dataset <- pd_long %>% group_by(name,genus) %>% summarize(mean_p = mean(value)) %>% arrange(desc(mean_p))
+
+prop_dataset <- prop_dataset %>% 
+  arrange(desc(mean_p))
+
+prop_dataset$name <- factor(prop_dataset$name, levels = prop_dataset$name)
+
+pdf("morphotype_dist/figures/tree_prop.pdf")
+prop_dataset %>% ggplot() + aes(x = name, y = mean_p*100, fill = genus) + geom_bar(stat = 'identity') + 
+  theme_classic() +
+  scale_x_discrete(guide = guide_axis(angle = 45)) +
+  ylab("% of total dataset") + xlab("") + 
+  theme(legend.position = 'top') +
+  scale_fill_discrete("")
+dev.off()
